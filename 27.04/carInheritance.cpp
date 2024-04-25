565703d26b4c36553f77b37a92351d907c2d6717
Inga <ingaserobya01@gmail.com>
2024-04-25 22:54:16 +0400

    draft

---
+++ 27.04/carInheritance.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class Car
+{
+public:
+	Car(const int& wheels, const int& seats, const std::string& engine) :	_wheels(wheels),
+														_seats(seats),
+														_engine(engine) {}
+private:
+	int	_wheels;
+	int _seats;
+	std::string	_engine;
+};
+
+/**
+				 ________________Car______________________
+				/			      |			              \
+			   / 			      |		  	               \
+			  /                   |                         \
+	Passengers				     Cargo				         working
+ _____|_______   		      ____|_____                _________|___________
+/     |       \              /    |     \              /         |           \
+bus   family    common    Light  Middle  Heavy  Agrigulture  Construction   Fisrt Aid
+
+*/
+
+
+int main()
+{
+
+	return 0;
+}
