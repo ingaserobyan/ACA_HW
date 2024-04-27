8c9c4d6255a0f1b28d24a1b98c259a8eb26eac97
Inga Serobyan <iserobya@c2r6s5.42yerevan.am>
2024-04-27 20:05:38 +0400

    test

---
+++ test.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+class A
+{
+	public:
+		A() : x(5) {}
+
+	public:
+		virtual void foo()
+		{
+			std::cout << "Class A " << this->x << std::endl;
+		}
+	private:
+		int x;
+};
+
+class B : public A
+{
+	public:
+		B() : x(6) {}
+	public:
+		void foo()
+		{
+			std::cout << "Class B " << this->x << std::endl;
+		}
+
+	private:
+		int x;
+};
+
+int main()
+{
+	A ob;
+	B obj;
+
+	ob.foo();
+	obj.foo();
+
+	std::cout << std::endl;
+
+	A* ptr1 = &obj;
+	ptr1->foo();
+
+
+
+	return (0);
+}
